b9d1bced24eb443ba4f0032db27c2c694ab85a87
MattIPv4 <matthew@cowley.org.uk>
2018-05-18 18:25:16 +0100

    add license comments

--- macOSNotif.js
+++ macOSNotif.js
@@ -1,3 +1,21 @@
+/**
+ *  macOSNotifJS: A simple Javascript plugin to create simulated macOS notifications on your website.
+ *  <https://github.com/MattIPv4/macOSNotifJS/>
+ *  Copyright (C) 2018 Matt Cowley (MattIPv4) (me@mattcowley.co.uk)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published
+ *   by the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *  You should have received a copy of the GNU General Public License
+ *   along with this program. If not, please see
+ *   <https://github.com/MattIPv4/macOSNotifJS/blob/master/LICENSE> or <http://www.gnu.org/licenses/>.
+ */
+
 class __macOSNotifJS_Interact {
     constructor(element) {
         this.element = typeof(element) === 'string' ? document.querySelector(element) : element;
@@ -120,7 +138,7 @@ async function __macOSNotifJS_loadTemplate() {
 
     // Get the template
     const response = await fetch(src);
-    __macOSNotifJS_template = await response.text();
+    __macOSNotifJS_template = (await response.text()).replace(/<!--[\s\S]*?(?:-->)/g, '');
 }
 
 function __macOSNotifJS_fullId(id) {
