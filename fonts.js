1d2e940c9c7206a56c865fdf68c3edb50275cc4b
MattIPv4 <matthew@cowley.org.uk>
2019-06-06 09:36:03 +0100

    Update fonts.js to resolve eslint issues

--- fonts.js
+++ fonts.js
@@ -4,10 +4,14 @@ const fonts = new Fontmin()
     .src("src/fonts/src/*.otf")
     .use(Fontmin.otf2ttf())
     .use(Fontmin.ttf2eot())
-    .use(Fontmin.ttf2woff({deflate: true}))
+    .use(Fontmin.ttf2woff({ deflate: true }))
     .dest("src/fonts/build");
 
 fonts.run(function (err, files) {
     if (err) throw err;
-    console.log(files.map(function (f) { return f.history.slice(-1)[0].replace(/^.*[\\\/]/, ''); }));
+    files.forEach(file => {
+        console.info("\n" + file.history.join("\n"));
+        console.log("\t" + file.history.slice(-1)[0].replace(/^.*[\\/]/, ""));
+    });
+    console.log("\nSuccessfully build " + files.length.toString(10) + " font files.");
 });
