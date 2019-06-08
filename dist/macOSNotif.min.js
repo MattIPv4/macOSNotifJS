b1166e3db8119641152ee5a0dce72d1a59b7ac4e
MattIPv4 <matthew@cowley.org.uk>
2019-06-08 12:23:28 +0100

    Begin version 0.1.0

--- dist/macOSNotif.min.js
+++ dist/macOSNotif.min.js
@@ -1,5 +1,5 @@
 /*!
- * macOSNotifJS - v0.0.6 - 2019-06-08
+ * macOSNotifJS - v0.1.0-beta1 - 2019-06-08
  * 
  * macOSNotifJS: A simple Javascript plugin to create simulated macOS notifications on your website.
  * <https://github.com/MattIPv4/macOSNotifJS/>
